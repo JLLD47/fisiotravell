2be7736d0eff1781273107f426b224cd9ea7ed58
JLLD47 <oormzoo@gmail.com>
2024-04-25 19:27:15 +0100

    minifix

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 
-const 
-{ signUp, logIn } = require("../controllers/auth.controller");
+const { signUp, logIn } = require("../controllers/auth.controller");
 
 router.post("/signup", signUp).post("/login", logIn);
 
-            module.exports = router;
+module.exports = router;
